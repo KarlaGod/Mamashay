67ace7557d88d8ae83d30e224e5c24f390a5a888
blacdav <davidaniefoik@gmail.com>
2024-03-25 11:47:47 +0100

    updated the chat but still working on it

--- app/[user]/[chats]/chat/page.jsx
+++ app/[user]/[chats]/chat/page.jsx
@@ -2,24 +2,20 @@
 import React, { Fragment, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { chat } from '../../data'
+import { chat, products } from '../../data'
 
-
-const Page = ({isVisible, onClose}) => {
-    const toggleShow = (e) => {
-        if(e.target.id === "wrapper") onClose();
-        console.log('done')
-    }
-
+const Page = () => {
+    // const [toggle, setToggle] = useState()
+    
   return (
-    <Fragment>
+    // <Fragment>
     <main className="min-h-screen bg-secondary text-colort">
       {
         chat.map((c, index) => {
             return(
                 <div className='w-screen h-screen' key={index}>
                     {/* Header of the Page */}
-                    <header className='grid px-3 bg-fborder'>
+                    <header className='grid px-3 z-10 bg-fborder'>
                         <div className='flex justify-between pt-5 items-center'>
                             <div className='flex items-center'>
                                 <div className='flex gap-3 py-3'>
@@ -40,7 +36,7 @@ const Page = ({isVisible, onClose}) => {
                         <div className='flex gap-2 py-5 overflow-x-scroll text-xs'>
                             <Link href='#' className='flex flex-none py-1 px-3 w-fit rounded-full bg-secondary gap-2'>
                                 <Image src={c.calender} alt='...' width={10} height={10} />
-                                <p className='border-l pl-2'>{}</p>
+                                <p className='border-l pl-2'>{c.address}</p>
                             </Link>
                             <Link href='tel:+2348077116265' className='flex flex-none py-1 px-3 w-fit rounded-full bg-secondary gap-2'>
                                 <Image src={c.tel} alt='...' width={10} height={10} />
@@ -54,18 +50,38 @@ const Page = ({isVisible, onClose}) => {
 
                         <div className='text-xs flex justify-between items-center pb-3 gap-3'>
                             <p className='w-4/5'><span className='font-bold'>Desciption: </span>{c.description}</p>
-                            <button id='wrapper' onClick={toggleShow} className='flex gap-1 p-2 bg-tertiary items-center text-secondary h-7 rounded-full w-1/5'>More <Image src={c.down} alt='...' width={10} height={10} className='animate-bounce' /></button>
+                            <button className='flex gap-1 p-2 bg-tertiary items-center text-secondary h-7 rounded-full w-1/5'>More <Image src={c.down} alt='...' width={10} height={10} className='animate-bounce' /></button>
                         </div>
-                        <div>
-                            
+                        <div className='grid'>
+                            <h1 className='flex items-center text-sm text-black font-semibold'><div className='h-2 w-2 bg-yellow rounded-full mr-1'></div> Other Products</h1>
+                            <div className='flex gap-2 py-2 overflow-x-scroll'>
+                            {
+                                products.map((product, index) => {
+                                return(
+                                    <div className='flex flex-none bg-secondary mx-1 px-2 py-2 w-45 rounded-lg' key={index}>
+                                        <Image src={product.img} alt='...' width={40} height={40} className='h-12 w-12 rounded-lg flex mr-2' />
+                                        <div className='text-xs grid'>
+                                            <p className='font-semibold text-black'>{product.user}</p>
+                                            <p>{product.ingredients}</p>
+                                            <div className='flex'>
+                                                <Image src={product.star} alt='...' width={10} height={10} className='mr-1' />
+                                                <p className='[5px]'>{product.rating}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                                })
+                            }
+                            </div>
                         </div>
+
                     </header>
 
-                    <div className='bg-fborder absolute bottom-0 pt-4 pb-6 px-3 gap-2 flex items-center w-screen justify-between'>
+                    <div className='bg-fborder absolute bottom-0 pt-4 pb-6 px-3 z-10 gap-2 flex items-center w-screen justify-between'>
                         <div className='flex justify-between bg-secondary p-1 rounded-full w-full'>
                             <div className='flex gap-2'>
                                 <button className='bg-fborder rounded-full p-2'><Image src={c.add_doc} alt='...' width={15} height={15} /></button>
-                                <input type="text" placeholder='Type message here' className='border-none' />
+                                <input type="text" placeholder='Type message here' className='outline-none w-full ' />
                             </div>
                             <Image src={c.placeholderimg} alt='...' width={20} height={15} className='mr-3' />
                         </div>
@@ -76,8 +92,8 @@ const Page = ({isVisible, onClose}) => {
         })
       }
     </main>
-    {/* <Showcase isVisible = {showModal} onClose = {() => setShowModal(false)} /> */}
-    </Fragment>
+    // <Showcase isVisible = {showModal} onClose = {() => setShowModal(false)} />
+    // </Fragment>
   )
 }
 
--- app/[user]/[chats]/page.jsx
+++ app/[user]/[chats]/page.jsx
@@ -5,8 +5,9 @@ import Image from 'next/image'
 import back from '@/public/homepage-img/back.svg'
 import { chats, navLinks2 } from '../data'
 import { usePathname } from 'next/navigation'
-import search from '@/public/homepage-img/search.svg'
+import search from '@/public/homepage-img/search2.svg'
 import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/router'
 
 const Page = () => {
   const pathname = usePathname();
@@ -16,8 +17,8 @@ const Page = () => {
 
   return (
     <main className='min-h-screen bg-secondary text-colort'>
-      <div>
-        <div className='flex justify-between items-center pt-5 bg-fborder h-[12%] w-screen fixed'>
+      <div className='bg-fborder pb-3'>
+        <div className='flex justify-between items-center py-5 h-[12%] w-screen'>
           <h1 className='flex items-center text-primary text-xl font-semibold pl-2'><Link href='../'><Image src={back} alt='...' width={25} height={25} className='pr-3' /></Link> Chats</h1>
           <div className='flex gap-2 p-3'>
             {
@@ -31,18 +32,19 @@ const Page = () => {
             }
           </div>
         </div>
-        <div className='bg-black z-30 rounded-full'>
-          <Image src={search} alt='...' width={15} height={15} />
+        <div className='bg-secondary p-2 mx-2 flex rounded-full text-xs items-center text-colort'>
+          <Image src={search} alt='...' width={15} height={15} className='mx-3 stroke-[#999999]' />
+          <input type="search" name="search" id="search" placeholder='Search any message here' className='outline-none w-full' />
         </div>
       </div>
 
       {/* Chat Lists */}
-      <div className='px-3 pt-20 pb-4 md:pt-28'>
+      <div className='px-3 pt-2 pb-4 md:pt-28'>
         <div>
           {
             chats.map((chat, index) => {
               return(
-                <div className='rounded-lg flex justify-between items-center pt-3 my-1' onClick={router.push('../chats/chat')} key={index}>
+                <div className='rounded-lg flex justify-between items-center pt-3 my-1' onClick={() => router.push('/user/chats/chat')} key={index}>
                   <div className='flex'>
                     <p className='rounded-full flex justify-center h-fit w-fit mr-2'><Image src={chat.icon} alt='...' width={40} height={40} className='rounded-full' /></p> 
                     <div className='flex justify-between items-center border-b pb-3'>
