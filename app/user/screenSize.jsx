dc5920962862e3172814246bd7b1b7c31381e4ef
blacdav <davidaniefoik@gmail.com>
2024-10-29 08:39:55 +0100

    updated the function to monitor screensize

--- app/user/screenSize.jsx
+++ app/user/screenSize.jsx
@@ -1,20 +1,33 @@
 "use client"
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const ScreenSize = ({ children }) => {
-    if(window.innerWidth > 425) {
+    const [isMobile, setIsMobile] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 425);
+        };
+        
+        handleResize(); // Check screen size initially
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    if (!isMobile) {
         return (
             <main className='flex bg-primary min-h-screen w-screen justify-center items-center'>
                 <p className='text-2xl text-center w-4/5'>We&apos;re Sorry, This page is not available to large screens, Please try using a mobile phone, or smaller screen</p>
             </main>
-        )
+        );
     }
 
-  return (
-    <>
-        { children }
-    </>
-  )
-}
+    return (
+        <>
+            {children}
+        </>
+    );
+};
 
-export default ScreenSize
+export default ScreenSize;
